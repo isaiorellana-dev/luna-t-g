fd82eda92cd8724fd276d2135c67b48a57636618
isai <isaiorellana.dev@gmail.com>
2022-03-07 14:36:02 -0600

    :bento: Fonts
    Fuentes agregadas y pre-configuradas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <React.Fragment>
       <h1>Hello World</h1>
+      <p>
+        Sitio web para el servicio de delivery <span>Luna To Go</span>
+      </p>
     </React.Fragment>
   );
 }
