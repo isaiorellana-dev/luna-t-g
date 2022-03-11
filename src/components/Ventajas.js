456f1881e2740cc4b6b253484c6014299cb9cc44
isai <isaiorellana.dev@gmail.com>
2022-03-11 15:40:10 -0600

    :sparkles: Slider y banner principal
    Slider para mobile de las ventajas e hice responsive el banner principal con las imagenes reales

--- src/components/Ventajas.js
+++ src/components/Ventajas.js
@@ -6,36 +6,115 @@ import profesional from "@icons/profesional.png";
 import "@styles/components/Ventajas.scss";
 
 const Ventajas = () => {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+
+  const isMobile = () => {
+    if (windowWidth < 450) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const clasesImg = ["img-1", "img-2", "img-3", "img-4", "img-5"];
+  const [indexImg, setIndexImg] = React.useState(0);
+
+  function next() {
+    indexImg === 0 ? setIndexImg(2) : setIndexImg(indexImg - 1);
+    return indexImg;
+  }
+
+  let timer;
+  const updateTimer = () => {
+    timer = setInterval(() => {
+      next();
+    }, 3500);
+  };
+
+  React.useEffect(() => {
+    updateTimer();
+
+    return () => clearInterval(timer);
+  }, [indexImg]);
+
   return (
     <section className="Ventajas__banner">
-      <div className="Ventajas__left">
-        <h1>Ventajas de utilizar nuestro servicio</h1>
-        {/* <Button
-          className="Ventajas__button"
-          text="llama ya!!"
-          link="Contacto"
-        /> */}
-      </div>
-      <div className="Ventajas__right">
-        <div>
-          <div className="seguro__icon Ventajas__circle">
-            <img src={seguro} alt="seguro" />
+      {!isMobile() && (
+        <React.Fragment>
+          <div className="Ventajas__left">
+            <h1>Ventajas de utilizar nuestro servicio</h1>
+            <Button
+              className="Ventajas__button"
+              text="llama ya!!"
+              link="Contacto"
+            />
           </div>
-          <h2>Seguro y confiable</h2>
-        </div>
-        <div>
-          <div className="profesional__icon Ventajas__circle">
-            <img src={profesional} alt="profesional" />
+          <div className="Ventajas__right">
+            <div>
+              <div className="seguro__icon Ventajas__circle">
+                <img src={seguro} alt="seguro" />
+              </div>
+              <h2>Seguro y confiable</h2>
+            </div>
+            <div>
+              <div className="profesional__icon Ventajas__circle">
+                <img src={profesional} alt="profesional" />
+              </div>
+              <h2>Servicio profesional</h2>
+            </div>
+            <div>
+              <div className="rapido__icon Ventajas__circle">
+                <img src={rapido} alt="rapido" />
+              </div>
+              <h2>Ahorras tiempo y esfuerzo</h2>
+            </div>
           </div>
-          <h2>Servicio profesional</h2>
-        </div>
-        <div>
-          <div className="rapido__icon Ventajas__circle">
-            <img src={rapido} alt="rapido" />
+        </React.Fragment>
+      )}
+      {isMobile() && (
+        <React.Fragment>
+          <div className="Ventajas__left">
+            <h1>Ventajas de utilizar nuestro servicio</h1>
           </div>
-          <h2>Ahorras tiempo y esfuerzo</h2>
-        </div>
-      </div>
+          <div className="Ventajas__right slider">
+            <div className="img--container">
+              <div className={`container ${clasesImg[indexImg]}`}>
+                <div className={`seguro__icon Ventajas__circle`}>
+                  <img src={seguro} alt="seguro" />
+                </div>
+                <h2>Seguro y confiable</h2>
+              </div>
+              <div
+                className={`container ${
+                  clasesImg[indexImg === 2 ? 0 : indexImg + 1]
+                } `}
+              >
+                <div className={`profesional__icon Ventajas__circle`}>
+                  <img src={profesional} alt="profesional" />
+                </div>
+                <h2>Servicio profesional</h2>
+              </div>
+              <div
+                className={`container ${
+                  clasesImg[indexImg === 0 ? 2 : indexImg - 1]
+                } `}
+              >
+                <div className={`rapido__icon Ventajas__circle`}>
+                  <img src={rapido} alt="rapido" />
+                </div>
+                <h2>Ahorras tiempo y esfuerzo</h2>
+              </div>
+            </div>
+            <Button
+              className="Ventajas__button"
+              text="llama ya!!"
+              link="Contacto"
+            />
+          </div>
+        </React.Fragment>
+      )}
     </section>
   );
 };
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -7,21 +7,6 @@ const Inicio = () => {
     <React.Fragment>
       <MainBanner />
       <Ventajas />
-      <h1>Todo a la puerta de tu casa</h1>
-      <h2>
-        Sitio web para el servicio de delivery <span>Luna To Go</span>
-      </h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit non ipsam
-        distinctio, molestiae ullam culpa dolorum consectetur at accusamus modi!
-        Ipsa natus excepturi in eaque cumque quae iste officiis laudantium et
-        quam ratione cum expedita debitis ipsam, quasi qui vel quos. Quae nihil
-        odit consequuntur minima. Ab ad iure aliquam nihil voluptates
-        perferendis ipsa eos temporibus et soluta molestiae, sequi iste quas,
-        totam reiciendis repellat nisi delectus natus cumque. Quis expedita,
-        accusantium repudiandae voluptatem maiores porro ipsum! Aspernatur,
-        accusamus? Debitis?
-      </p>
     </React.Fragment>
   );
 };
