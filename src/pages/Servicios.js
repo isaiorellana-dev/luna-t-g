9aaa402374829c823afac8be0da045c262894bff
David <davidnunez0494@gmail.com>
2022-03-21 14:50:44 -0600

    :speech_balloon: Agregar texto en Servicios
    Se actualizaron los textos en el apartado de servicios

--- src/pages/Servicios.js
+++ src/pages/Servicios.js
@@ -10,27 +10,41 @@ const Servicios = () => {
         <h1>Nuestros Servicios:</h1>
         <div className="servicios__first-container">
           <ServiciosCard
+            titulo="Comida"
+            text="Te llevamos tu comida hasta la puerta de tu casa ya sea que tengas 
+            un pedido listo o nosotros mismos lo hacemos por ti, nos especializamos en cuidar 
+            la calidad y presentación de tus pedidos, además de seguridad brindamos
+             rapidez para que tu comida llegue como recién salida de la cocina y puedas
+              disfrutarla al máximo, para hacer uso de nuestro servicio solo hace falta
+               tener hambre y nosotros hacemos el resto."
+            img={chavito}
+          />
+          <ServiciosCard
             titulo="Envíos"
-            text="lorem ipsum dolor sit amet consectetur adipiscing elit vehicula
-            inceptos praesent sollicitudin iaculis imperdiet donec auctor
-            potenti aenean rhoncus euismod diam elementum parturient integer
-            convallis feugiat"
+            text="Llevamos tus pedidos hasta su destino, de manera rápida y segura,
+            hacemos realidad un método efectivo para que puedas enviar tus productos
+            o paquetería sin ningún tipo de preocupación, nos especializamos en la 
+            seguridad del producto hasta que llegue a su consumidor final, puedes 
+            utilizar nuestro servicio como cliente y vendedor, para realizar un 
+            envío solo hace falta un paquete y un destino, nosotros hacemos el resto."
             img={chavito}
           />
           <ServiciosCard
             titulo="Compras"
-            text="lorem ipsum dolor sit amet consectetur adipiscing elit vehicula
-            inceptos praesent sollicitudin iaculis imperdiet donec auctor
-            potenti aenean rhoncus euismod diam elementum parturient integer
-            convallis feugiat"
+            text="Realizamos tus compras de todo tipo, te ofrecemos un servicio de 
+            calidad y confianza para realizar tus compras en supermercado, bodegas, 
+            farmacias, ferreterías y tiendas de todo tipo, somos tu mejor opción para 
+            que no salgas de casa y realices tus compras de una manera fácil y segura,
+             para realizar una compra solo hace falta una lista de productos y nosotros 
+             hacemos el resto."
             img={chavito}
           />
           <ServiciosCard
             titulo="Transacciones Bancarias"
-            text="lorem ipsum dolor sit amet consectetur adipiscing elit vehicula
-            inceptos praesent sollicitudin iaculis imperdiet donec auctor
-            potenti aenean rhoncus euismod diam elementum parturient integer
-            convallis feugiat"
+            text="Si necesitas realizar un deposito o tramite bancario que no necesite 
+            de tu presencia, te ahorramos esas largas filas y hacemos tus tramites, 
+            nos comprometemos a velar por la seguridad de tus documentos, brindamos 
+            un servicio profesional y serio.."
             img={chavito}
           />
         </div>
@@ -39,12 +53,11 @@ const Servicios = () => {
         <div>
           <h2>Horarios</h2>
           <p>
-            lorem ipsum dolor sit amet consectetur adipiscing elit vehicula
-            inceptos praesent sollicitudin iaculis imperdiet
+            De Lunes a Domingo de 9 A.M a 9 P.M
           </p>
         </div>
         <div>
-          <h2>Que esperas?</h2>
+          <h2>¿Qué esperas?</h2>
           <Button
             className="MainBanner__button"
             text="Llama Ya!!"
