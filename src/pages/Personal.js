6e128a770c784bec2a231a95eacc2b26e7ea34ce
isai <isaiorellana.dev@gmail.com>
2022-03-30 15:26:28 -0600

    ⚡️ Cambiando assets en Inicio y personal

--- src/pages/Personal.js
+++ src/pages/Personal.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PersonalBanner from "@components/PersonalBanner";
-import fotoUno from "@img/personal-1.avif";
-import fotoDos from "@img/personal-2.avif";
-import fotoTres from "@img/personal-3.avif";
-import fotoCuatro from "@img/personal-4.avif";
-import fotoCinco from "@img/personal-5.avif";
-import fotoSeis from "@img/personal-6.avif";
+import fotoUno from "@img/personal-1.png";
+import fotoDos from "@img/personal-2.png";
+import fotoTres from "@img/personal-3.png";
+import fotoCuatro from "@img/personal-4.png";
+import fotoCinco from "@img/personal-5.png";
+import fotoSeis from "@img/personal-6.png";
 import "@styles/pages/Personal.scss";
 
 const Personal = () => {
