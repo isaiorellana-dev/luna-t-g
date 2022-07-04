9d8a4fe38e53aec74825d0499afaeaa7a818ef2b
isai <isaiorellana.dev@gmail.com>
2022-07-04 10:58:32 -0600

    Merge con info_promo

--- src/promos/promos.js
+++ src/promos/promos.js
@@ -1,18 +1,42 @@
 export const promos = [
-  {
-    nombre: "",
-    descripcion: "",
-    from: "",
-    fecha: "",
-    precio: "",
-    img: ""
-  },
-  {
-    nombre: "",
-    descripcion: "",
-    from: "",
-    fecha: "",
-    precio: "",
-    img: "../assets/img/lafoto.jpg"
-  },
+   {
+      nombre: "ALITAS",
+      descripcion: "Alitas a L10 c/u + salsa de maracuyá (Papas + L35)",
+      from: "Chrys",
+      fecha: "4/7/22",
+      precio: "L 10 c/u",
+      img: "../assets/img/chrys-promo.jpg"
+   },
+   {
+      nombre: "LUNA FINGERS",
+      descripcion: "4 Fingers + Salsa + 1 Papas o tajadas",
+      from: "Marc s Fingers",
+      fecha: "5/7/22",
+      precio: "L 80",
+      img: "../assets/img/fingers-promo.jpg"
+   },
+   {
+      nombre: "PINCHOS 1806",
+      descripcion: "2 Pinchos + Frijoles + Extras",
+      from: "Super Pollo 1806",
+      fecha: "6/7/22",
+      precio: "L 140",
+      img: "../assets/img/spollo-promo.jpg"
+   },
+   {
+      nombre: "COMBO LUNA",
+      descripcion: "Pizza suprema + 1 Orden pan de ajo + 1 Refresco pepsi litro 1/4",
+      from: "Pizzeria KYDMC",
+      fecha: "7/7/22",
+      precio: "L 325",
+      img: "../assets/img/kydmc-promo.jpg"
+   },
+   {
+      nombre: "BABY RIBS EN SALSA BBQ",
+      descripcion: "Puré de papa + Ensalada + Arroz + Tortilla o tostones de plátano verde + Bebida gratis",
+      from: "Mary s Grill",
+      fecha: "8/7/22",
+      precio: "L 160",
+      img: "../assets/img/marys-promo.jpg"
+   },
 ]
